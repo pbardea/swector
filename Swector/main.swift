ff99fd509b85355202b1ae3250eb4cf72d5b5782
Paul Bardea <paul@pbardea.com>
2016-05-10 09:38:50 -0400

    Initial Commit

---
+++ Swector/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swector
+//
+//  Created by Paul Bardea on 2016-05-10.
+//  Copyright © 2016 pbardea. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
